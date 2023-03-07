1b6a44ba89a35fd42b3da2599658844c17f04aa8
keyonime <126454960+keyonime@users.noreply.github.com>
2023-03-07 16:52:42 +0800

    Update sales.py

--- sales.py
+++ sales.py
@@ -10,24 +10,22 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LinearRegression
 
 st.write("""
 # Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
+This app predicts the **Sales**!
 """)
 
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
+    TV = st.sidebar.slider('TV', 0, 200, 10)
+    Radio = st.sidebar.slider('Radio', 0, 100, 10)
+    Newspaper = st.sidebar.slider('Newspaper', 0, 100, 10)
+    data = {'TV': TV,
+            'Radio': Radio,
+            'Newspaper': Newspaper,
     features = pd.DataFrame(data, index=[0])
     return features
 
@@ -36,21 +34,22 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+dfdata = pd.read_csv('Adversiting.csv')
+dfdata = dfdata.drop([Unnamed: 0'],axis=1)
+X = dfdata.drop(['Sales'],axis=1)
+Y = dfdata.Sales
 
-clf = RandomForestClassifier()
+clf = LinearRegression()
 clf.fit(X, Y)
 
 prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+st.write(dfdata.target_names)
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
+st.write(dfdata.target_names[prediction])
 #st.write(prediction)
 
 st.subheader('Prediction Probability')
